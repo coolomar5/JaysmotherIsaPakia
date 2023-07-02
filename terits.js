4e3cc94deb706f149cb301336c1b4f80d6ffaf22
coolomar5 <136412677+coolomar5@users.noreply.github.com>
2023-07-02 19:23:21 +0200

    Add files via upload

---
+++ terits.js
@@ -0,0 +1,20 @@
+"use strict";
+let canvas = document.getElementById("tetris");
+const context = document.getElementById("2d");
+
+context.scale = (20, 20)
+
+function arenaSweep(){
+    let rowCount = 1;
+    outer: for(let y  = arena.Length; y>0; --y){s
+        for(let x = 0; x<arena[y].Length; ++x){
+            if(arena[y][x]==0){
+                continue outer;
+        }
+        
+        }
+
+        
+    }
+}
+
